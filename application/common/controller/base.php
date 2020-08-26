e130fd39c22589b1b6a892f38bed7e0f6d1046c1
WRS <1195965103@qq.com>
2020-08-26 17:29:04 +0800

    20200826 手机页面notice控制器继承公共控制器（app\common\controller\base.php）ok

---
+++ application/common/controller/base.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\common\controller;
+
+use think\Controller;
+
+/*
+移动端页面控制器基类 
+ */
+
+class Base extends Controller
+{
+	protected $ajaxAuthErr = 0; //ajax请求时 （未登录时：not_login, 无权限时：not_auth)
+	
+	protected function initialize ()
+	{
+		//if ( 未登录 ) $ajaxAuthErr = 'not_login'
+		//halt('ini');
+	}
+}
--- application/index/controller/Notice.php
+++ application/index/controller/Notice.php
@@ -2,9 +2,9 @@
 namespace app\index\controller;
 
 use think\Db;
-use think\Controller;
+use app\common\controller\base;
 
-class Notice extends Controller
+class Notice extends Base
 {	
     //通知公告首页
     public function index()
@@ -15,5 +15,46 @@ class Notice extends Controller
     		return view();
     		//return view()->assign(['ip' => $ip, 'stu_num' => $stu_num, 'stu_name' => $stu_name, 'type' => $type]);
     	}        
+    }
+	
+	//显示公告添加页
+    public function add_page()
+    {
+		return view('add');
+    }
+	
+	//执行添加
+    public function add()
+    {
+		$d = input();
+		
+		$msg = '';
+		
+		if ( !(isset($d['title']) && mb_strlen($d['title']) >= 5 && mb_strlen($d['title']) <= 20)  )
+		{
+			$msg .= '标题应为5~20字！';
+		}
+		
+		if ( !(isset($d['content']) && mb_strlen($d['content']) >= 10 && mb_strlen($d['content']) <= 150)  )
+		{
+			$msg .= '内容应为5~20字！';
+		}
+		
+		if ( $msg != '' )
+		{
+			return json(['code' =>100, 'msg' => $msg]);
+		}
+		
+		$d['time'] = date('Y-m-d H:i:s', time());
+		$d['adder'] = '管理员';//后期改为实际登录者 session
+		
+		$r = Db::table('tp_notice')->insert($d);
+		
+		if ( $r )
+		{
+			return json(['code' =>200, 'msg' => '发布成功']);
+		}else{
+			return json(['code' =>100, 'msg' => '发布失败']);
+		}
     }
 }
